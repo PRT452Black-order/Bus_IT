dbe6d5c5bf692348b39d43dc08229501f7845a53
NB106 <NB106@users.noreply.github.com>
2018-09-19 21:05:43 +0930

    Front End Setup
    This is just a layout setup for frontend which can be used develop furthur content. All the pages and navigations has been added.

---
+++ app/src/main/java/com/example/cdu_nb/bus_it/HelpFrag.java
@@ -0,0 +1,17 @@
+package com.example.cdu_nb.bus_it;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class HelpFrag extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater,ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View myview=inflater.inflate(R.layout.help,container,false);
+        return myview;
+    }
+}
---
+++ app/src/main/java/com/example/cdu_nb/bus_it/TopUpFrag.java
@@ -0,0 +1,17 @@
+package com.example.cdu_nb.bus_it;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class TopUpFrag extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater,ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View myview=inflater.inflate(R.layout.topup,container,false);
+        return myview;
+    }
+}
