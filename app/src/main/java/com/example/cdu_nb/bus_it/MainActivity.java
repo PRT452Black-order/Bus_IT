78853be8f762ec5e46bb38933a452d16b4292806
PatrickA7 <patrick.alegria7@gmail.com>
2018-09-21 10:49:26 +0930

    Make search bus page default when logging in

--- app/src/main/java/com/example/cdu_nb/bus_it/MainActivity.java
+++ app/src/main/java/com/example/cdu_nb/bus_it/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.FragmentManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentTransaction;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -20,6 +21,9 @@ public class MainActivity extends AppCompatActivity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FragmentTransaction tx = getSupportFragmentManager().beginTransaction();
+        tx.replace(R.id.main, new SearchBusFrag());
+        tx.commit();
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
